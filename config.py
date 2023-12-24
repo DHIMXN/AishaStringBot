79d85404916751abb96d9897ea6a7ef915d0a454
Dhiman / <154400066+DHIMXN@users.noreply.github.com>
2023-12-24 15:00:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,3 +13,6 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 OWNER_ID = int(getenv("OWNER_ID", 5437285949))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Dhiman_Network")
+
+
+START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/25efe6aa029c6baea73ea.jpg")
