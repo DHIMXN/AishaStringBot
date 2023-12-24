dc23abbada5bac775d0430d62f2f70f6bbaa320f
Dhiman / <154400066+DHIMXN@users.noreply.github.com>
2023-12-24 15:07:34 +0530

    Update start.py

--- StringGen/modules/start.py
+++ StringGen/modules/start.py
@@ -9,6 +9,8 @@ from StringGen.utils import add_served_user, keyboard
 async def f_start(_, message: Message):
     await message.reply_text(
         text=f"ʜᴇʏ {message.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {Anony.mention},\nsᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.",
+        await message.reply_photo(
+            photo=config.START_IMG_URL,
         reply_markup=keyboard,
         disable_web_page_preview=True,
     )
